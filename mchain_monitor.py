9959f89838946b490f93caf1e4c86a252d703c15
KartelAG <KartelAG@cbr.ru>
2019-11-04 01:33:35 +0500

    critical edit

--- mchain_monitor.py
+++ mchain_monitor.py
@@ -10,46 +10,49 @@ from systemstats import SystemStats
 
 class Mchain_Monitor():
     def __init__(self):
-        self.error_log = open(self.__config['error_filename', 'a'])
         try:
             self.__config = configparser.ConfigParser()
             self.__config.read('config.ini')
-            if (self.__config['stat_dest'] == 'file'):
-                self.logger = LoggerToFile(self.__config['filename'])
+            self.error_log = open(self.__config['global']['error_filename'], 'a')
+            if (self.__config['global']['stat_dest'] == 'file'):
+                self.logger = LoggerToFile(self.__config['global']['filename'])
         except:
             self.error_log.write(''.join(str(datetime.datetime.now()), ' Error while init! Check config.ini and stat_dest option'))
             sys.exit()
         try:
-            self.w3 = Web3(Web3.WebsocketProvider('ws://' + self.__config['api_address'] + ':' + self.__config['api_port']))
+            self.w3 = Web3(Web3.WebsocketProvider('ws://' + self.__config['global']['api_address'] + ':' + self.__config['global']['api_port']))
         except:
-            self.error_log.write(''.join(str(datetime.datetime.now()), ' cannot connect to mchain node via ', self.__config['api_address'], ':', self.__config['api_port']))
+            self.error_log.write(''.join(str(datetime.datetime.now()), ' cannot connect to mchain node via ', self.__config['global']['api_address'], ':', self.__config['global']['api_port']))
         
-    def log_event(event_data):
+    def log_event(self, event_data):
         self.logger.log_event(event_data)
 
-    def log_loop(event_filter, poll_interval):
+    def log_loop(self, event_filter, poll_interval):
         while True:
             for event in event_filter.get_new_entries():
-                self.log_event(event)
+                self.log_event(str(event.hex()))
             time.sleep(poll_interval)
 
-    def subscribe_for_new_blocks():
+    def subscribe_for_new_blocks(self):
         self.block_filter = self.w3.eth.filter('latest')
-        self.log_loop(self.block_filter, 0)
+        self.log_loop(self.block_filter, 1)
 
-    def run():
-        self.worker = Thread(target=subscribe_for_new_blocks, daemon=True)
+    def run(self):
+        self.worker = Thread(target=self.subscribe_for_new_blocks, daemon=True)
         self.worker.start()
 
 
-class LoggerToFile(filename):
+class LoggerToFile():
     def __init__(self, filename):
-        try:
-            self.__fd == open(filename, 'a')
-        except:
-            self.error_log.write(''.join(str(datetime.datetime.now()), ' Error opening log file!'))
-            sys.exit()
+        self.filename = filename
+        self.fd = open(self.filename, 'a')
     
-    def log_event(event_data):
-        self.__fd.write(event_data)
+    def log_event(self, event_data):
+        self.fd.write(event_data)
+        self.fd.write('\n')
 
+
+if __name__ == '__main__':
+    obj = Mchain_Monitor()
+    #obj.run()
+    obj.subscribe_for_new_blocks()
